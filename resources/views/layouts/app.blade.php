1a176af957c82c6d71fa5b0fdd4087eeaaad7a04
m55 <m55@localhost.localdomain>
2018-02-12 02:18:59 +0300

    main template done

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>@yield('title')</title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+    <!-- CSS Files -->
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="/assets/css/now-ui-kit.css" rel="stylesheet" />
+    <!-- CSS Just for demo purpose, don't include it in your project -->
+    <link href="/assets/css/demo.css" rel="stylesheet" />
+    <!--  Social tags      -->
+    <meta name="keywords" content="@yield('keywords')">
+    <meta name="description" content="@yield('description')">
+</head>
+
+<body class="landing-page">
+@include('includes.header')
+<div class="wrapper">
+@yield('content')
+@include('includes.footer')
+</div>
+</body>
+<!--   Core JS Files   -->
+<script src="/assets/js/core/jquery.3.2.1.min.js" type="text/javascript"></script>
+<script src="/assets/js/core/tether.min.js" type="text/javascript"></script>
+<script src="/assets/js/core/popper.min.js" type="text/javascript"></script>
+<script src="/assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+<script src="/assets/js/plugins/moment.min.js"></script>
+<!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+<script src="/assets/js/plugins/bootstrap-switch.js"></script>
+<!--	Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
+<script src="/assets/js/plugins/bootstrap-tagsinput.js"></script>
+<!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
+<script src="/assets/js/plugins/bootstrap-selectpicker.js" type="text/javascript"></script>
+<!--  Google Maps Plugin    -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDGat1sgDZ-3y6fFe6HD7QUziVC6jlJNog"></script>
+<!--	Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
+<script src="/assets/js/plugins/jasny-bootstrap.min.js"></script>
+<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+<script src="/assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+<!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
+<script src="/assets/js/plugins/bootstrap-datetimepicker.min.js" type="text/javascript"></script>
+<!-- Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc -->
+<script src="/assets/js/now-ui-kit.js" type="text/javascript"></script>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,247 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
+@section('title')
+Fuckstrot.com
+@endsection 
+@extends('layouts.app')
+@section('content')
+        <div class="page-header page-header-mini">
+            <div class="page-header-image" data-parallax="true" style="background-image: url('../assets/img/bg26.jpg');">
+            </div>
+            <div class="content-center">
+                <h1 class="title">This is our great company.</h1>
+                <div class="form-group hidden-xs hidden-sm">
+                                                <input type="text" value="" placeholder="Search something interesting" class="form-control">
+                                            </div>
+            </div>
+        </div>
+        <div class="section section-about-us">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 offset-md-2 text-center">
+                        <h2 class="title">Who we are?</h2>
+                        <h5 class="description">According to the National Oceanic and Atmospheric Administration, Ted, Scambos, NSIDClead scentist, puts the potentially record low maximum sea ice extent tihs year down to low ice extent in the Pacific and a late drop in ice extent in the Barents Sea.</h5>
+                    </div>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="separator separator-primary"></div>
+                <div class="section-story-overview">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <!-- First image on the left side -->
+                            <div class="image-container image-left" style="background-image: url('../assets/img/bg38.jpg')">
+                                <p class="blockquote blockquote-primary">"Over the span of the satellite record, Arctic sea ice has been declining significantly, while sea ice in the Antarctichas increased very slightly"
+                                    <br>
+                                    <br>
+                                    <small>-NOAA</small>
+                                </p>
+                            </div>
+                            <!-- Second image on the left side of the article -->
+                            <div class="image-container image-left-bottom" style="background-image: url('../assets/img/bg24.jpg')"></div>
+                        </div>
+                        <div class="col-md-5">
+                            <!-- First image on the right side, above the article -->
+                            <div class="image-container image-right" style="background-image: url('../assets/img/bg39.jpg')"></div>
+                            <h3>So what does the new record for the lowest level of winter ice actually mean</h3>
+                            <p>The Arctic Ocean freezes every winter and much of the sea-ice then thaws every summer, and that process will continue whatever happens with climate change. Even if the Arctic continues to be one of the fastest-warming regions of the world, it will always be plunged into bitterly cold polar dark every winter. And year-by-year, for all kinds of natural reasons, there’s huge variety of the state of the ice.
+                            </p>
+                            <p>
+                                For a start, it does not automatically follow that a record amount of ice will melt this summer. More important for determining the size of the annual thaw is the state of the weather as the midnight sun approaches and temperatures rise. But over the more than 30 years of satellite records, scientists have observed a clear pattern of decline, decade-by-decade.
+                            </p>
+                            <p>The Arctic Ocean freezes every winter and much of the sea-ice then thaws every summer, and that process will continue whatever happens with climate change. Even if the Arctic continues to be one of the fastest-warming regions of the world, it will always be plunged into bitterly cold polar dark every winter. And year-by-year, for all kinds of natural reasons, there’s huge variety of the state of the ice.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+        <div class="testimonials-1 section-image" style="background-image: url('../assets/img/bg19.jpg')">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 offset-md-3 text-center">
+                        <h2 class="title">What is ALPHA?</h2>
+                        <h4 class="description text-white">If you’re selected for ALPHA you’ll also get 3 tickets, opportunity to access Investor Office Hours and Mentor Hours and much more all for €850.</h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card card-testimonial">
+                            <div class="card-avatar">
+                                <a href="landing-page.html#pablo">
+                                    <img class="img img-raised" src="../assets/img/michael.jpg">
+                                </a>
+                            </div>
+                            <div class="card-block">
+                                <p class="card-description">
+                                    The networking at Web Summit is like no other European tech conference.
+                                </p>
+                            </div>
+                            <div class="icon icon-primary">
+                                <i class="fa fa-quote-right"></i>
+                            </div>
+                            <div class="card-footer">
+                                <h4 class="card-title">Michael Elijah</h4>
+                                <p class="category">@michaelelijah</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card card-testimonial">
+                            <div class="card-avatar">
+                                <a href="landing-page.html#pablo">
+                                    <img class="img img-raised" src="../assets/img/olivia.jpg">
+                                </a>
+                            </div>
+                            <div class="card-block">
+                                <p class="card-description">
+                                    The connections you make at Web Summit are unparalleled, we met users all over the world.
+                                </p>
+                            </div>
+                            <div class="icon icon-primary">
+                                <i class="fa fa-quote-right"></i>
+                            </div>
+                            <div class="card-footer">
+                                <h4 class="card-title">Olivia Harper</h4>
+                                <p class="category">@oliviaharper</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card card-testimonial">
+                            <div class="card-avatar">
+                                <a href="landing-page.html#pablo">
+                                    <img class="img img-raised" src="../assets/img/james.jpg">
+                                </a>
+                            </div>
+                            <div class="card-block">
+                                <p class="card-description">
+                                    Web Summit will increase your appetite, your inspiration, and your network.
+                                </p>
+                            </div>
+                            <div class="icon icon-primary">
+                                <i class="fa fa-quote-right"></i>
+                            </div>
+                            <div class="card-footer">
+                                <h4 class="card-title">James Logan</h4>
+                                <p class="category">@jameslogan</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="pricing-2">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 offset-md-3 text-center">
+                        <h2 class="title">Pick the best plan for you</h2>
+                        <ul class="nav nav-pills nav-pills-primary justify-content-center" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" data-toggle="tab" href="landing-page.html#pill1" role="tablist">
+                                    Legal Entity
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-toggle="tab" href="landing-page.html#pill2" role="tablist">
+                                    Individual
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card card-pricing card-plain">
+                            <div class="card-block">
+                                <h6 class="category">Enterprise</h6>
+                                <h1 class="card-title">
+                                    <small>$</small>59</h1>
+                                <ul>
+                                    <li>
+                                        <b>10GB</b> Disk Space</li>
+                                    <li>
+                                        <b>100GB</b> Monthly Bandwidth</li>
+                                    <li>
+                                        <b>20</b> Email Accounts</li>
+                                    <li>
+                                        <b>Unlimited</b> subdomains</li>
+                                </ul>
+                                <a href="landing-page.html#pablo" class="btn btn-primary btn-round">
+                                    Sign Up
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card card-pricing card-background card-raised" style="background-image: url('../assets/img/pricing2.jpg')">
+                            <div class="card-block">
+                                <h6 class="category text-info">Professional</h6>
+                                <h1 class="card-title">
+                                    <small>$</small>29</h1>
+                                <ul>
+                                    <li>
+                                        <b>5GB</b> Disk Space</li>
+                                    <li>
+                                        <b>50GB</b> Monthly Bandwidth</li>
+                                    <li>
+                                        <b>10</b> Email Accounts</li>
+                                    <li>
+                                        <b>Unlimited</b> subdomains</li>
+                                </ul>
+                                <a href="landing-page.html#pablo" class="btn btn-neutral btn-round">
+                                    Sign Up
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card card-pricing card-plain">
+                            <div class="card-block">
+                                <h6 class="category">Standard</h6>
+                                <h1 class="card-title">
+                                    <small>$</small>17</h1>
+                                <ul>
+                                    <li>
+                                        <b>2GB</b> Disk Space</li>
+                                    <li>
+                                        <b>25GB</b> Monthly Bandwidth</li>
+                                    <li>
+                                        <b>5</b> Email Accounts</li>
+                                    <li>
+                                        <b>Unlimited</b> subdomains</li>
+                                </ul>
+                                <a href="landing-page.html#pablo" class="btn btn-primary btn-round">
+                                    Get Started
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="section section-contact-us text-center">
+            <div class="container">
+                <h2 class="title">Want to work with us?</h2>
+                <p class="description">Your project is very important to us.</p>
+                <div class="row">
+                    <div class="col-lg-6 text-center offset-lg-3 col-md-8 offset-md-2">
+                        <div class="input-group input-lg">
+                            <span class="input-group-addon">
+                                <i class="now-ui-icons users_circle-08"></i>
+                            </span>
+                            <input type="text" class="form-control" placeholder="First Name...">
+                        </div>
+                        <div class="input-group input-lg">
+                            <span class="input-group-addon">
+                                <i class="now-ui-icons ui-1_email-85"></i>
+                            </span>
+                            <input type="text" class="form-control" placeholder="Email...">
+                        </div>
+                        <div class="textarea-container">
+                            <textarea class="form-control" name="name" rows="4" cols="80" placeholder="Type a message..."></textarea>
+                        </div>
+                        <div class="send-button">
+                            <a href="landing-page.html#pablo" class="btn btn-primary btn-round btn-block btn-lg">Send Message</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+@endsection
